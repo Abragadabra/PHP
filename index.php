d80669696dad7b7f9b24013a6ec17875b7ec60cc
Alexander <saychamini@gmail.com>
2021-03-15 22:26:14 +0500

    Data Types

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP</title>
+</head>
+<body>
 <?php
+    $hello = 'hello';
+    $world = 'world';
+    $number = 548787;
+    $number2 = -487.48;
+    $bool = true;
+    echo $hello . ' ' . $world . '. ' . $number . ' ' . $number2 . '<br>';
+    echo $bool . ' Это булевое значение =)';
+?>
+</body>
+</html>
