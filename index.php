f8652698bbd89fc076297b0505df67653f4e4f13
Alexander <saychamini@gmail.com>
2021-03-15 22:58:05 +0500

    math action2

--- index.php
+++ index.php
@@ -29,9 +29,7 @@
 //    define(NUMBER_PI, 3.14159265); // создал свою первую константу
 
     $a = 15;
-    $b = 30;
-
-    $a += 10;
+    $b = -30;
 
 //    echo $a + $b . '<br>';
 //    echo $a - $b . '<br>';
@@ -40,7 +38,13 @@
 //    echo $a % $b . '<br>';
 
     echo M_PI . '<br>';
-    echo M_E;
+    echo M_E . '<br>';
+
+    echo abs($b) . '<br>';
+    echo ceil(3.001) . '<br>'; // округляет к большему
+    echo floor(9.99999). '<br>'; // округляет к меньшему
+    echo round(3.456, 1) . '<br>'; // правильное округление
+    echo mt_rand(-999, 999) . '<br>';
 ?>
 </body>
 </html>
