bc7f6fab296d8725aaa9fae271b133bdb65ee795
Alexander <saychamini@gmail.com>
2021-03-15 22:32:22 +0500

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <title>PHP</title>
+    <style>
+        body {
+            font-size: 35px;
+            font-family: sans-serif;
+            background-color: gray;
+        }
+    </style>
 </head>
 <body>
 <?php
@@ -12,7 +19,12 @@
     $number2 = -487.48; // float
     $bool = true; // boolean
     echo $hello . ' ' . $world . '. ' . $number . ' ' . $number2 . '<br>';
-    echo $bool . ' Это булевое значение =)';
+    echo $bool . ' Это булевое значение =) <br>';
+
+    $a = 5;
+    $b = "75";
+
+    echo $a + intval($b);
 ?>
 </body>
 </html>
