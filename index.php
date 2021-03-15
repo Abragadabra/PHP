d6ae19c7fd90c8a14fb11c5851de8f2997e552c6
Alexander <saychamini@gmail.com>
2021-03-15 22:39:58 +0500

    math action

--- index.php
+++ index.php
@@ -13,20 +13,22 @@
 </head>
 <body>
 <?php
-    $hello = 'hello'; // string
-    $world = 'world'; // string
-    $number = 548787; // int
-    $number2 = -487.48; // float
-    $bool = true; // boolean
-    echo $hello . ' ' . $world . '. ' . $number . ' ' . $number2 . '<br>';
-    echo $bool . ' Это булевое значение =) <br>';
+//    $hello = 'hello'; // string
+//    $world = 'world'; // string
+//    $number = 548787; // int
+//    $number2 = -487.48; // float
+//    $bool = true; // boolean
+//    echo $hello . ' ' . $world . '. ' . $number . ' ' . $number2 . '<br>';
+//    echo $bool . ' Это булевое значение =) <br>';
+//
+//    $a = 5;
+//    $b = "75";
+//
+//    echo $a + intval($b);
+//
+//    define(NUMBER_PI, 3.14159265); // создал свою первую константу
 
-    $a = 5;
-    $b = "75";
 
-    echo $a + intval($b);
-
-    define(PI, 3.14159265); // создал свою первую константу
 ?>
 </body>
 </html>
